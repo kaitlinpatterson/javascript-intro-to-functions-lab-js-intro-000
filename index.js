70e7d1c564291d1029eb843cf376987fbc179dae
Kaitlin Patterson <kaitlin.patterson@observepoint.com>
2019-07-29 20:36:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,4 +13,6 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
 if (string === 'I love you, Grandma.') {return "I love you, too."}
+else if (string === lowercase) {return "I can't hear you!"}
+else if (string === uppercase) {return "YES INDEED!"}
 }
