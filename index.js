d0eb46046271693304262f1b50eae2fb345f7e8f
Kaitlin Patterson <kaitlin.patterson@observepoint.com>
2019-07-29 20:37:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 var uppercase = "HELLO!"
+var lowercase = "hello!"
 function sayHiToGrandma(string) {
 if (string === 'I love you, Grandma.') {return "I love you, too."} 
 else if (string === lowercase) {return "I can't hear you!"} 
